5ec69d15f56327dc2d23a02df458caf4777a1d27
Your Name <harshdeshmukh278@gmail.com>
2024-01-12 11:34:23 +0530

    Added Empty Board

--- taskify-frontend/src/App.js
+++ taskify-frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import EmptyBoard from "./components/EmptyBoard.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <EmptyBoard type='add'/>
     </div>
   );
 }
