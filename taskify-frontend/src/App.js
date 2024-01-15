368fd36bf06cd76ac6006a5eb8d176624d0b2da7
Your Name <harshdeshmukh278@gmail.com>
2024-01-15 10:43:18 +0530

    Show Empty Board if no Board is added else show Boards which are added.

--- taskify-frontend/src/App.js
+++ taskify-frontend/src/App.js
@@ -1,12 +1,40 @@
 
 import './App.css';
 import EmptyBoard from "./components/EmptyBoard.js";
+import { useDispatch, useSelector } from "react-redux";
+import boardsSlice from './redux/boardsSlice.js';
+import { useState } from 'react';
+import Home from './components/Home.js';
+import AddEditBoardModal from './modals/AddEditBoardModal.js';
 
 
 function App() {
+
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+
   return (
     <div className=" overflow-hidden  overflow-x-scroll">
-      <EmptyBoard type='add'/>
+      {boards.length > 0 ?
+        <>
+        {/* <Header
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        /> */}
+        <Home
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        />
+        </>
+        :
+        <>
+          <EmptyBoard type='add'/>
+        </>
+      }
     </div>
   );
 }
